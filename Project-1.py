4433f09fbce787154f36c911e525b67e67db1c6c
mususer <109802062+mususer@users.noreply.github.com>
2023-02-17 15:51:36 +0530

    first day project

---
+++ Project-1.py
@@ -0,0 +1,12 @@
+# 100 days of Python Project
+# Project 1 -- Band name generator
+
+#greetings for the project
+print("Hello welcome to the band name generator project")
+
+#Ask the user about the city they grew up in
+city = input("Which city you grow up in ?")
+#Ask about their first school name
+name = input("What is your first school name ?")
+#Concantenate the strings and print the output
+print("Your band name could be" + " " + city + " " + name)
